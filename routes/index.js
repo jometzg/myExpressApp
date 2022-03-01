1c870f04d098ce0bc9386d0ef62603c51bf49696
John Metzger <56820659+jometzg@users.noreply.github.com>
2022-03-01 10:51:31 +0000

    use a variable

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var titletext = process.env.TITLE
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: titletext });
 });
 
 module.exports = router;
